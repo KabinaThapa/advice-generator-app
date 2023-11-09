813d5f5d0a4512422433dac16867f1327c8e895f
Kabina Thapa <thapakabina28@gmail.com>
2023-11-09 18:31:59 -0500

    style: update UI design and layout

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function handleClick(){
     fetch('https://api.adviceslip.com/advice')//fetch from the api url
     .then((response)=>response.json())
     .then(data=>{
-        console.log(data.slip.advice)
+        console.log(data)
+        const adviceid=data.slip.id
         const advice=data.slip.advice
-        document.getElementById('result').textContent= advice
+        document.getElementById('advice').textContent= advice
+        document.getElementById('adviceid').textContent= adviceid
+        const adviceElement=  document.getElementById('advice')
+        adviceElement.style.display=(adviceElement.style.display==='none')?'block':'none'
 
 
     })
